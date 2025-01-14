6487f11111af8bc80e268c273d6fb8cf6c547cac
techieadi4703 <techie.adi47@gmail.com>
2025-01-14 16:47:27 +0530

    imporved homePage

--- clientSide/Components/Home.jsx
+++ clientSide/Components/Home.jsx
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './hello.css'
 const Home = () => {
+  const [username, setUsername] = useState('');
+  const [showname,setShowname]= useState(false);
+    useEffect(() => {
+      callHome();
+    }, []);
+
+
+  const callHome = async () => {
+    try {
+      console.log("Fetching contact data...");
+      const response = await fetch("http://localhost:3000/contact", {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+        },
+        credentials: "include",
+      });
+      const data = await response.json();
+      console.log("Fetched data in home:", data);
+      const{name}=data;
+      setUsername(name);
+      setShowname(true);
+    } catch (err) {
+      console.error("Error fetching contact page:", err);
+    }
+  };
+
   return (
     <>
       <div className="home-page">
         <div className="home-div">
-          <p className="pt-5">WELCOME</p>
-          <h1>We are The MERN Developers</h1>
+          {/* <p className="pt-5">WELCOME</p> */}
+          <h1>Welcome {username}</h1>
+          <h2 className="pt-3">{showname?"Happy to see u again":"We are The MERN Developers"}</h2>
         </div>
       </div>
     </>
